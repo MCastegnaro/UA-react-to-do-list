96dced83b54702a51f4806f41cdbc596848aea8f
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-24 09:23:40 -0300

    adicionado teste de renderização do logo presente no componente header

--- src/pages/ToDo/components/Header/__test__/Header.test.tsx
+++ src/pages/ToDo/components/Header/__test__/Header.test.tsx
@@ -3,9 +3,14 @@ import { Header } from "..";
 import { describe, it, expect } from "vitest";
 
 describe("<Header />", () => {
-  it("Deve renderizar o header corretamente", () => {
-    render(<Header />);
-    const header = screen.getByTestId("header");
-    expect(header).not.toBeNull();
-  });
+    it("Deve renderizar o header corretamente", () => {
+        render(<Header />);
+        const header = screen.getByTestId("header");
+        expect(header).not.toBeNull();
+    });
+
+    it('Deve renderizar a imagem da logo no header corretamente', () => {
+        const img = screen.getByAltText("Logo principal do sistema");
+        expect(img).not.toBeNull();
+    })
 });
