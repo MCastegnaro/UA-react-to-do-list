9f6b97830bf595384d245a1d1a652e3a3d00452f
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-24 13:07:07 -0300

    adicionado testes das funções de remover uma tarefa por id e alterar o status da tarefa por id, na page ToDo no packge helpers

--- src/pages/ToDo/helpers/__test__/helpers.test.tsx
+++ src/pages/ToDo/helpers/__test__/helpers.test.tsx
@@ -1,5 +1,6 @@
-import { createNewTask } from "..";
+import { createNewTask, removeTask, toggleTaskStatus } from "..";
 import { describe, expect, it } from "vitest";
+import { Task } from "../../../../models/Task";
 
 describe("createNewTask", () => {
   it("Deve criar uma nova tarefa com um ID, descrição e isDone definido como false", () => {
@@ -9,5 +10,34 @@ describe("createNewTask", () => {
     expect(task.id).toBeDefined();
     expect(task.description).toBe(description);
     expect(task.isDone).toBe(false);
+  });
+
+  it("Deve remover uma tarefa da lista de tarefas com base no ID fornecido", () => {
+    const taskIdToRemove = "task2";
+    const taskList: Task[] = [
+      { id: "task1", description: "Task 1", isDone: false },
+      { id: taskIdToRemove, description: "Task 2", isDone: true },
+      { id: "task3", description: "Task 3", isDone: false },
+    ];
+
+    const updatedTaskList = removeTask(taskList, taskIdToRemove);
+
+    expect(updatedTaskList.length).toBe(taskList.length - 1);
+    expect(updatedTaskList.find((task) => task.id === taskIdToRemove)).toBeUndefined();
+  });
+
+  it("Deve alternar o status isDone de uma tarefa com base no ID fornecido", () => {
+    const taskIdToToggle = "task2";
+    const taskList: Task[] = [
+      { id: "task1", description: "Task 1", isDone: false },
+      { id: taskIdToToggle, description: "Task 2", isDone: true },
+      { id: "task3", description: "Task 3", isDone: false },
+    ];
+
+    const updatedTaskList = toggleTaskStatus(taskList, taskIdToToggle);
+    const toggledTask = updatedTaskList.find((task) => task.id === taskIdToToggle);
+
+    expect(toggledTask).toBeDefined();
+    expect(toggledTask!.isDone).toBe(!taskList.find((task) => task.id === taskIdToToggle)!.isDone);
   });
 });
