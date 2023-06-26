aabdaf0734ce869b487c1ec6f0c9a2d512a05bf0
Maria Damo <mdamodacosta@yahoo.com.br>
2023-06-25 23:19:48 -0300

    arrumando testes para NoContent component

--- src/pages/ToDo/components/NoContent/index.tsx
+++ src/pages/ToDo/components/NoContent/index.tsx
@@ -5,7 +5,7 @@ export const NoContent = () => {
   return (
     <section className={styles.section_container}>
       <img src={Clipboard} alt="ícone de clipboard" />
-      <p className={styles.text}>
+      <p className={styles.text} data-testid="texto">
         <strong> Você ainda não tem tarefas cadastradas</strong>
         Crie tarefas e organize seus itens a fazer
       </p>
