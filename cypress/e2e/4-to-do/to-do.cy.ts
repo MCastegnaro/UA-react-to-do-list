0492765470c1b4363d2806bf68ed54960d186d61
Lucaslmp77 <lucas.lmp77@gmail.com>
2023-06-28 10:37:15 -0300

    Teste da página ToDo, deve exibir um botão de criar uma nova tarefa

--- cypress/e2e/4-to-do/to-do.cy.ts
+++ cypress/e2e/4-to-do/to-do.cy.ts
@@ -17,5 +17,10 @@ describe('example to-do app', () => {
         it('Deve exibir um campo de entrada(input) para adicionar uma nova tarefa', () => {
             cy.get('._input_x3dtl_21').should("be.visible");
         });
+
+        it('Deve exibir um botão de criar uma nova tarefa', () => {
+            cy.get('._button_x3dtl_75').should("be.visible");
+            cy.get('._button_x3dtl_75 > img').should("be.visible");
+        });
     });
 });
