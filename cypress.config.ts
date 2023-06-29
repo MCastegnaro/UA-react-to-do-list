fd38f1443efcf7ff1ef7080d8af9ba077c3bd396
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-06-28 22:12:30 -0300

    iniciando testes e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+      import('@cypress/code-coverage/task').then((module) => {
+        module.default(on, config);
+      });
 
+      return config
     },
-
   },
 });
