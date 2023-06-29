403147a62a92fbc20f04ef604d128ad96b377394
JoaoSchulz <joaoguilherme.da@gmail.com>
2023-06-28 23:10:14 -0300

    testes e2e inicial

--- cypress/e2e/4-to-do/to-do.cy.ts
+++ cypress/e2e/4-to-do/to-do.cy.ts
@@ -1,37 +1,11 @@
-describe('<Landing Page>', () => {
-
-  context('Quando acessar a home do projeto', () => {
-    it('Deve exibir uma tag h1 com título do sistema', () => {
-      cy.viewport("macbook-15")
-      cy.visit('http://localhost:3000')
-      cy.get('h1').should("contain.text", "To do List")
-    })
-
-    it('Deve exibir uma tag h2 com subtítulo do sistema', () => {
-      cy.viewport("macbook-15")
-      cy.visit('http://localhost:3000')
-      cy.get('h2').should("contain.text", "Desafio do Ignite")
-    })
-
-    it('Deve exibir uma imagem de banner', () => {
-      cy.viewport("macbook-15")
-      cy.visit('http://localhost:3000')
-      cy.get('img').should("be.visible")
-    })
-
-    describe("E clicar na imagem de banner", () => {
-      it('Deve navegar para a listagem de tarefas ', () => {
-        cy.viewport("macbook-15")
-        cy.visit('http://localhost:3000')
-
-        cy.get('img').should("be.visible")
-
-        cy.get('img').click()
-
-        cy.url().should("include", "/to-do");
-
-      })
-    })
-
+describe('<ToDo>', () => {
+  beforeEach(() =>{
+    cy.viewport("macbook-15"); //abre a tela com o tamanho de um macbook-15
+    cy.visit('http://127.0.0.1:3000'); //entrar no site pelo ip
+    cy.get('img').should("be.visible"); //verificar se a imagem (capa) está visivel
+    cy.get('img').click(); //clicar na imagem para entrar no to-do
+    cy.url().should("include", "/to-do"); //incluir o /to-do na url
   })
+
+  
 })
