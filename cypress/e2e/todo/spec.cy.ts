b7847fea2e4e88d4f730b14f8540ece4ff60cfa8
Gabriel Verle <nykogabriel@gmail.com>
2023-06-28 21:57:11 -0300

    E2E tests of input & button logic

--- cypress/e2e/todo/spec.cy.ts
+++ cypress/e2e/todo/spec.cy.ts
@@ -24,4 +24,14 @@ describe('test To Do page', () => {
     cy.get(':nth-child(2) > ._span_value_x3dtl_191').should("be.visible");
   });
 
+  it('should disable create button when input is empty', () => {
+    cy.get('._input_x3dtl_21').should("be.visible");
+    cy.get('._button_x3dtl_75').should("be.visible").should("be.disabled");
+  });
+
+  it('should enable create button when input has content', () => {
+      cy.get('._input_x3dtl_21').should("be.visible").type("Nova tarefa");
+      cy.get('._button_x3dtl_75').should("be.visible").should("not.be.disabled");
+  });
+
 })
