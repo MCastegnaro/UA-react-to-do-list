d1e4a28fbbadc93144829eaf4f07e7ea6254aba0
jeann <jeannguilherme@hotmail.com>
2023-06-30 18:42:07 -0300

    Entrega 2

--- src/pages/ToDo/components/Header/index.tsx
+++ src/pages/ToDo/components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 
 export const Header = () => {
   return (
-    <header data-testid="header" className={styles.header}>
+    <header data-test-id="header" className={styles.header}>
       <img className={styles.img} src={logo} alt="Logo principal do sistema" />
     </header>
   );
